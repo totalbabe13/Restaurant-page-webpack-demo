9696b6855369e17081fb464a52ceb6fd435c77c1
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-21 12:59:22 -0500

    adding all files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('Yolo Binches!');
+alert("yolo test 1")
+// function component() {
+//   const element = document.createElement('div');
+//   element.innerHTML = 'Hello webpack'
+//   return element;
+// }
+
+ import { testLayout } from './modules/layout.js';
+
+ testLayout();
