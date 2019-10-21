cc75bd880dce09289900f4bde89baf8ba44c1ecb
leonardo Kaplan <kaplanleonardo@gmail.com>
2019-10-21 12:03:01 -0500

    added header using layout module, and imported to index

--- src/modules/layout.js
+++ src/modules/layout.js
@@ -1,5 +1,14 @@
 
 const testLayout = (() => {
+
    document.body.setAttribute("background","rest_home.jpeg");
+
+   let header = document.createElement("h1");
+   header.textContent = "Welcome to Soup D'jour!"
+   header.setAttribute("style", "width: 100vw; background-color: #f2794478; color: white; font-family: cursive; font-size: 3em; text-align: center;")
+
+   document.body.appendChild(header);
+
+
 })
 export {testLayout};
